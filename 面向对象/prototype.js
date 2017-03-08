731d32f11d0507bd12d926cdac4933c7593620b2
slipkinem <398873901@qq.com>
2017-03-08 19:27:36 +0800

    pubSub

--- 面向对象/prototype.js
+++ 面向对象/prototype.js
@@ -1,68 +1,80 @@
 var object = {
-	isA : function (aType) {
-		// body...
-		var self = this;
-		console.info(this);
-		while(self){
-            if (self == aType)
-                return true;
-            self = self.Type;
-        }
-        return false;
-	}
+  isA: function (aType) {
+    // body...
+    var self = this;
+    console.info(this);
+    while (self) {
+      if (self == aType)
+        return true;
+      self = self.Type;
+    }
+    return false;
+  }
 };
-
-function Class(aBaseClass,aClassDefine) {
-    function class_() {
-        this.Type = aBaseClass;
-        for (var member in aClassDefine){
-            this[member] = aClassDefine[member];
-        }
-    }
-    class_.prototype = aBaseClass;
-    return new class_();
+class Listen {
+  constructor(height, width) {
+    this.height = height
+    this.width = width
+  }
 }
 
-function New(aClass,aParams) {
-    function new_() {
-        this.Type = aClass;
+let listen = new Listen(23, 34)
+console.log(listen)
 
-        if (aClass.Create)
-            aClass.Create.apply(this,aParams);
+
+function Class(aBaseClass, aClassDefine) {
+  function class_() {
+    this.Type = aBaseClass;
+    for (var member in aClassDefine) {
+      this[member] = aClassDefine[member];
     }
-    new_.prototype = aClass;
-    return new new_();
+  }
+
+  class_.prototype = aBaseClass;
+  return new class_();
+}
+
+function New(aClass, aParams) {
+  function new_() {
+    this.Type = aClass;
+
+    if (aClass.Create)
+      aClass.Create.apply(this, aParams);
+  }
+
+  new_.prototype = aClass;
+  return new new_();
 }
 
 
-var Person = Class(object,{
-   Create: function (name,age) {
-        this.name = name;
-        this.age = age;
-   },
-    SayHello: function () {
-        console.dir('this.name= ' + this.name +' this.age= ' + this.age);
-   }
+var Person = Class(object, {
+  Create: function (name, age) {
+    this.name = name;
+    this.age = age;
+  },
+  SayHello: function () {
+    console.dir('this.name= ' + this.name + ' this.age= ' + this.age);
+  }
 });
 
-var Employee = Class(Person,{
-    Create:function (name,age,salary) {
-        Person.Create.call(this,name,age);
-        this.salary = salary;
-    },
-    ShowMeTheMoney: function () {
-        console.dir(this.name + ' $ ' + this.salary);
-    }
+var Employee = Class(Person, {
+  Create: function (name, age, salary) {
+    Person.Create.call(this, name, age);
+    this.salary = salary;
+  },
+  ShowMeTheMoney: function () {
+    console.dir(this.name + ' $ ' + this.salary);
+  }
 });
 
-var lvsen = New(Person,["lvsen",43]);
-var zhangsan = New(Employee,["zhangsan",53,345]);
+var lvsen = New(Person, ["lvsen", 43]);
+var zhangsan = New(Employee, ["zhangsan", 53, 345]);
 
 lvsen.SayHello();
 zhangsan.SayHello();
 zhangsan.ShowMeTheMoney();
 
-var smallLvsen = New(lvsen.Type,['smallLvsen',34]);
+var smallLvsen = New(lvsen.Type, ['smallLvsen', 34]);
 smallLvsen.SayHello();
 
 console.dir(lvsen.isA(Person));
