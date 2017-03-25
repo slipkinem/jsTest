b19719e06ab3deb805a4193c237f8371e7bf33e6
slipkinem <13087553662@qq.com>
2017-03-25 20:07:55 +0800

    暂写promise

--- promise/promise05.js
+++ promise/promise05.js
@@ -4,11 +4,48 @@
 'use strict'
 
 function Promise(resolver) {
-  
+
+  this.deferreds = []
+  this.rejects = []
+  var _this = this
+
   function resolve(value) {
-    this.defrreds = []
+    // 为了让.then先执行，防止非异步时resolve先执行，.then的回调没有注册进入deferred
+    setTimeout(function () {
+      _this.deferreds.forEach(function (deferred) {
+        deferred(value)
+      })
+    }, 0)
   }
-  
-  resolver(resolve)
-  
+
+  function reject(value) {
+    _this.rejects.forEach(function(reject) {
+      reject(value)    
+    })
+
+  }
+
+  resolver(resolve, reject)
+
 }
+
+Promise.prototype.then = function (onSuccess, onRejected) {
+  this.deferreds.push(onSuccess)
+  this.rejects.push(onRejected)
+  return this
+}
+
+
+new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve('haha')
+    })
+  })
+  .then(function (result) {
+    console.log(result)
+  }, function(err) {
+    console.log(err)
+  })
+  .then(function(value) {
+    console.log(value)
+  })
