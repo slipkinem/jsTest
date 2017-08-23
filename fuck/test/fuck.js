b8d04bc427a99a82afe29084aaae8a1343104859
slipkinem <398873901@qq.com>
2017-08-23 17:50:14 +0800

    add chapter20

---
+++ fuck/test/fuck.js
@@ -0,0 +1,25 @@
+// #!/usr/bin/env node
+// var fs = require('fs'),
+//   path = process.cwd()
+// var appInfo = require('../package.json')
+//
+// function app (o) {
+//   if (o[0] === '-v' || o[0] === '--version') {
+//     console.log('  version is ' + appInfo.version)
+//   } else if (o[0] === '-h' || o[0] === '--help') {
+//     console.log('Useage:')
+//     console.log('  -v --version [show version]')
+//   } else {
+//     fs.readdir(path, function (err, files) {
+//       if (err) return console.error(err)
+//       for (var i = 0; i < files.length; i++) {
+//         console.log(files[i])
+//       }
+//     })
+//   }
+// }
+// // argv 命令行参数排列 比如：node app.js --fuck
+// // argv => [命令符node路径, 执行app.js路径， --fuck]
+// app(process.argv.slice(2))
+// console.log(process.argv.slice(2))
+console.log('tetwsetetewt%dsfsdfsdf%dsf'.split('%'))
---
+++ javaScript设计模式读书笔记/chapter20.js
@@ -0,0 +1,4 @@
+/**
+ * Created by slipkinem on 8/23/2017 at 5:05 PM.
+ */
+'use strict'
