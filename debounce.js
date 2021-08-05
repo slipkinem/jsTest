fd16019ddd00c7b2225d201bcc562e8d4fcd2bcc
sen.lv <sen.lv@zhangmen.com>
2021-08-05 14:38:39 +0800

    feat: add debounce

--- debounce.js
+++ debounce.js
@@ -1,3 +1,13 @@
 /**
  * Created by sen.lv on 2021/8/5 at 13:56.
  */
+function debounce (fn, delay = 500) {
+  let timeId = 0;
+  return (...args) => {
+    if (timeId) clearTimeout(timeId)
+
+    timeId = setTimeout(() => {
+      fn.apply(this, args)
+    }, delay)
+  }
+}
--- throttle.js
+++ throttle.js
@@ -1,3 +1,27 @@
 /**
  * Created by sen.lv on 2021/8/5 at 13:57.
  */
+// function throttle (fn, delay = 500) {
+//   let timeId = 0;
+//
+//   return function(...args) {
+//     if (!timeId) {
+//       setTimeout(() => {
+//         fn.apply(this, args)
+//         timeId = 0;
+//       }, delay)
+//     }
+//   }
+// }
+
+function throttle (fn, delay = 500) {
+  let time = Date.now();
+
+  return function (...args) {
+    let now = Date.now()
+    if (now - time >= delay) {
+      fn.apply(this, args)
+      time = now;
+    }
+  }
+}
